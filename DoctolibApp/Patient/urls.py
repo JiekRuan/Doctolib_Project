96135f959ae64a5ea204a288941f456373276e8f
Jiek Ruan <72401198+JiekRuan@users.noreply.github.com>
2025-02-11 00:58:03 +0100

    Suppression de la version non fonctionnelle

--- DoctolibApp/Patient/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'Patient'
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('dashboard/', views.patient_dashboard, name='patient_dashboard'),
-]
